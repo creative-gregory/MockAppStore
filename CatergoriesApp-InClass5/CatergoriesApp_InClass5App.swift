eeeb4011f1c5c1ce3e3d6d3454dfe9504f2701ee
Gregory Hagins <gahagins@gmail.com>
2023-11-07 06:13:05 -0500

    Initial Commit

---
+++ CatergoriesApp-InClass5/CatergoriesApp_InClass5App.swift
@@ -0,0 +1,17 @@
+//
+//  CatergoriesApp_InClass5App.swift
+//  CatergoriesApp-InClass5
+//
+//  Created by Gregory Hagins II on 11/7/23.
+//
+
+import SwiftUI
+
+@main
+struct CatergoriesApp_InClass5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
